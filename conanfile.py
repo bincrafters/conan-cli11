3240d10367e634d53a038324d8b9e08f3739ea2a
sigmoidal <stathis@npcglib.org>
2018-01-04 04:23:25 +0200

    Fixing c++11 definitions for consumers.

--- conanfile.py
+++ conanfile.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from conans import ConanFile, tools
+from conans.model.version import Version
 import os
-
 
 class LibnameConan(ConanFile):
     name = "cli11"
@@ -12,16 +12,29 @@ class LibnameConan(ConanFile):
     url = "https://github.com/bincrafters/conan-cli11"
     license = "BSD 3-Clause"
     exports = ["LICENSE.md"]
+    settings = "compiler"
 
     def source(self):
         source_url = "https://github.com/CLIUtils/CLI11"
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
         extracted_dir = "CLI11-" + self.version
         os.rename(extracted_dir, "sources")
-
+    
     def package(self):
         self.copy(pattern="LICENSE", src="sources")
         self.copy(pattern="*", dst="include", src="sources/include")
 
     def package_id(self):
         self.info.header_only()
+
+    def package_info(self):
+        v = Version(str(self.settings.compiler.version))
+        if self.settings.compiler == "gcc":
+            if v >= "5":
+                self.cpp_info.defines = [ '_GLIBCXX_USE_CXX11_ABI=1' ]
+            else:
+                self.cpp_info.defines = [ '_GLIBCXX_USE_CXX11_ABI=0' ]
+
+        if str(self.settings.compiler) in [ "gcc", "apple-clang", "clang" ]:
+            self.cpp_info.cppflags = [ "-std=c++11" ]
+
