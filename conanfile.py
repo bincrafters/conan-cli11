5b5158a7988cc3a5f7fb623886bd2ed774a9b1b9
Uilian Ries <uilianries@gmail.com>
2018-04-22 08:51:27 -0300

    #237 Bump version to 1.5.3
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ import os
 
 class Cli11Conan(ConanFile):
     name = "cli11"
-    version = "1.4.0"
+    version = "1.5.3"
     description = "Command line parser for C++11"
     url = "https://github.com/bincrafters/conan-cli11"
     license = "BSD 3-Clause"
@@ -20,7 +20,7 @@ class Cli11Conan(ConanFile):
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
         extracted_dir = "CLI11-" + self.version
         os.rename(extracted_dir, self.source_subfolder)
-    
+
     def package(self):
         include_dir = os.path.join(self.source_subfolder, "include")
         self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
@@ -28,4 +28,3 @@ class Cli11Conan(ConanFile):
 
     def package_id(self):
         self.info.header_only()
-
