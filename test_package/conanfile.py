ff41a3d3d48da02be1ac3db7722f9edd827b6015
Uilian Ries <uilianries@gmail.com>
2018-04-22 08:52:21 -0300

    #237 Print CLI during test package
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -16,10 +16,11 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         with tools.environment_append(RunEnvironment(self).vars):
+            help_option = "--help"
             bin_path = os.path.join("bin", "test_package")
             if self.settings.os == "Windows":
-                self.run(bin_path)
+                self.run("%s %s" % (bin_path, help_option))
             elif self.settings.os == "Macos":
-                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
+                self.run("DYLD_LIBRARY_PATH=%s %s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path, help_option))
             else:
-                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
+                self.run("LD_LIBRARY_PATH=%s %s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path, help_option))
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,7 +1,10 @@
+#include <string>
 #include "CLI/CLI.hpp"
 
 int main(int argc, char **argv)
 {
     CLI::App app("K3Pi goofit fitter");
+    app.set_footer("Bincrafters");
+    app.set_help_flag("--help", "A help string");
     CLI11_PARSE(app, argc, argv);
 }
